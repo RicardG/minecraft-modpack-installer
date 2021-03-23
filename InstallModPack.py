7d092e2d5d99f0bb75ab594bcfcdc566c21af954
Ricard Grace <ricard.grace@student.unsw.edu.au>
2021-03-23 17:32:34 +1100

    Upload

---
+++ InstallModPack.py
@@ -0,0 +1,386 @@
+#!/usr/bin/python3
+#curse forge modpack installer by Ricard Grace
+import sys;
+import json;
+import zipfile;
+import os;
+import urllib.request;
+import urllib.error;
+import re;
+import shutil;
+import subprocess;
+import time;
+import threading;
+
+tempDir = "modtemp";
+manifestLoc = os.path.join(tempDir, "manifest.json");
+modsLoc = os.path.join(tempDir, "mods");
+minecraftProfLoc = os.path.join(os.getenv("APPDATA"), os.path.join(".minecraft", "launcher_profiles.json"));
+
+def main():
+    print("Minecraft modpack installer by Ricard Grace");
+
+    #read in the manifest file that will be provided as the second arg, right?
+    #do checking of input arguments
+    if (len(sys.argv) < 2):
+        print("This program needs to be run from the command line. (Double clicking on it does not work!)");
+        print("Usage: " + sys.argv[0] + " <mod.zip>");
+        input("Press enter to continue...");
+        sys.exit();
+
+    if (not os.path.exists(sys.argv[1])):
+        print("File '" + sys.argv[1] + "' does not exist!");
+
+    nozip = False;
+    nodown = False;
+    noforge = False;
+    noprofile = False;
+    for i in range(2, len(sys.argv)):
+        if (sys.argv[i] == "-nozip"):
+            nozip = True;
+        elif (sys.argv[i] == "-nodown"):
+            nodown = True;
+        elif (sys.argv[i] == "-noforge"):
+            noforge = True;
+        elif (sys.argv[i] == "-noprofile"):
+            noprofile = True;
+
+    if (not nozip):
+        #delete the tempDir folder if it exists
+        if (os.path.isdir(tempDir)):
+            shutil.rmtree(tempDir);
+            time.sleep(2);
+        os.mkdir(tempDir);
+        #the file we are given is a zip package
+        #open the zip
+        print("Unzipping " + sys.argv[1] + "...");
+        zip_ref = zipfile.ZipFile(sys.argv[1], 'r')
+        zip_ref.extractall(tempDir);
+        zip_ref.close()
+        print("\tDone!\n");
+
+    #now to do file processing
+    exists = os.path.isfile(manifestLoc);
+    if (not exists):
+        print("Manifest file cannot be found. Exiting...");
+        sys.exit();
+
+    try:
+        f = open(manifestLoc);
+    except:
+        print("Could not open manifest file. Exiting...");
+
+    try:
+        j = json.load(f);
+    except:
+        print("File '" + manifestLoc + "' is not a valid json file!");
+        sys.exit();
+    finally:
+        f.close();
+
+    minecraftVersion = j["minecraft"]["version"];
+    forgeVersion = re.findall(r'[0-9.]*$', j["minecraft"]["modLoaders"][0]["id"])[0];
+    name = j["name"];
+    modVersion = j["version"];
+    gamedir = os.path.join(os.getenv("APPDATA"), ".minecraft", name);
+    print("Installing Modpack '"+name+"'");
+    print("\tVersion: " + modVersion);
+    print("\tMinecraft: " + minecraftVersion);
+    print("\tForge: " + forgeVersion);
+    while (True):
+        print("\nIs this correct? (y/n)");
+        t = input();
+        if (t in ("y", "Y")):
+            break;
+        elif (t in ("n", "N")):
+            print("Exiting...");
+            sys.exit();
+        else:
+            continue;
+
+    if (not nodown):
+        #time to download all the mods
+        #generate a list of mods we need to get and their urls
+        print("\nGenerating mod urls...");
+        fileList = [];
+        for mod in j["files"]:
+            fileList.append((str(mod["projectID"]),str(mod["fileID"])));
+        print("\tDone!");
+        numMods = len(fileList);
+        print(f"\tFound {numMods} mods");
+
+        print("\nDownloading mods:");
+        #create the folder we are going to store the mods in if it does not exist
+        if (os.path.isdir(modsLoc)):
+            #delete it and remake it
+            shutil.rmtree(modsLoc);
+            time.sleep(2);
+        os.mkdir(modsLoc);
+
+        #thread code here
+        errorList = [];
+        modListLock = threading.Lock();
+        errorListLock = threading.Lock();
+        data = DownloadThreadData(fileList, errorList, modListLock, errorListLock)
+        #spawn 4 threads
+        for i in range(0, 4):
+            thr = threading.Thread(target=DownloadModsThread, args=(data,));
+            thr.setDaemon(True);
+            thr.start();
+
+        #wait for all the threads to finish
+        while (True):
+            if (data.fileDone >= data.fileCount):
+                #then we have finished, display the exit message
+                print("Done!\t\t\t\t\t\t");
+                break;
+            else:
+                #display the progress message and then wait
+                time.sleep(0.1);
+                numBars = int(20*data.fileDone/data.fileCount);
+                progressString = "\t{0}/{1}\t[{2}{3}] {4}%".format(str(data.fileDone), str(data.fileCount), "|"*(numBars), " "*(20-numBars), str(int(100*data.fileDone/data.fileCount)));
+                print(progressString, end="\r")
+
+        errorcount = len(errorList);
+        if (errorcount > 0):
+            print("\nFailed to download " + str(errorcount) + " mods");
+            for e in errorList:
+                print(e);
+        input("Press enter to continue...");
+
+    if (not noforge):
+        #all the mods have been aquired, now to get forge and install it
+        forgename = 'forge-'+minecraftVersion+'-'+forgeVersion+'-installer-win.exe';
+        forgeurl = 'https://files.minecraftforge.net/maven/net/minecraftforge/forge/'+minecraftVersion+'-'+forgeVersion+'/'+forgename;
+        print("\nDownloading " + forgename);
+        try:
+            rec = urllib.request.Request(forgeurl, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'});
+            r = urllib.request.urlopen(rec);
+        except urllib.error.HTTPError as err:
+            print("Error downloading forge. Exiting...");
+            print(err.fp.read());
+            sys.exit();
+
+        forgeF = open(os.path.join(tempDir, forgename), "wb");
+        forgeF.write(r.read());
+        forgeF.close();
+
+        print("Running forge installer (It should come up as a seperate window)");
+        ret = subprocess.call(os.path.join(tempDir, forgename));
+        if (ret != 0):
+            print("Error installing forge");
+            sys.exit();
+
+        input("Press enter when forge has finished installing to continue...\n");
+
+    if (not noprofile):
+        #now that forge has been installed, create a new profile based on the forge one
+        profname = "modpack - " + name;
+        print("Adding/Replacing profile '"+profname+"'");
+        if (not os.path.exists(minecraftProfLoc)):
+            print("WHERE...");
+            print("IS YOUR MINECRAFT PROFILE!!!");
+            sys.exit();
+        #find the forge version to ensure that it was actually installed
+        forgevname = minecraftVersion+'-forge'+minecraftVersion+'-'+forgeVersion;
+        print("\tUsing forge version: " + forgevname);
+        if (not os.path.isdir(os.path.join(os.getenv('APPDATA'), ".minecraft", "versions", forgevname))):
+            print("Could not find required forge version (did you install it?). Exiting...");
+            sys.exit();
+
+        #the profile is where we expect it to be, make a new profile (or overwrite a previous one if the user is stupid enough to name their profile after a mod pack)
+        profF = open(minecraftProfLoc, "r");
+        profiles = json.load(profF);
+        profF.close();
+        #add the new profile
+        if ("profiles" not in profiles):
+            #create the profile dict
+            profiles["profiles"] = {};
+        profiles["profiles"][profname] = {};
+        profiles["profiles"][profname]["name"] = profname;
+        profiles["profiles"][profname]["gameDir"] = gamedir;
+        profiles["profiles"][profname]["lastVersionId"] = forgevname
+        #check how much memory the user wants to allocate
+        print("How much memory do you expect this modpack to use (in GB)? Usually it will say on the modpack page the recommended value.")
+        try:
+            mem = float(input("Enter a number between 1-32 (default 4)\n"));
+        except:
+            mem = 4;
+        if (mem < 1):
+            mem = 4;
+            print("Invalid value! Setting memory to default of 4GB");
+        elif (mem > 32):
+            mem = 4;
+            print("Invalid value! Setting memory to default of 4GB");
+        else:
+            print("Using " + str(mem) + "GB of memory");
+
+        mem = int(mem*1024);
+        profiles["profiles"][profname]["javaArgs"] = "-Xms"+str(mem)+"m -Xmx"+str(mem)+"m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC";
+        profF = open(minecraftProfLoc, "w");
+        json.dump(profiles, profF, indent=4);
+        profF.close();
+
+    #now to 'install' the modpack by copying over the mods folder and any folder spacified in the overrides
+    print("\nInstalling mods");
+    #make sure the directory we want to write to exists
+    if (not os.path.exists(gamedir)):
+        os.mkdir(gamedir);
+
+    overridesloc = os.path.join(tempDir, "overrides");
+    print("\tRemoving old files");
+    #for each folder in the overides folder, delete the corresponding folder in the game directory
+    overrideslist = os.listdir(overridesloc);
+    for d in overrideslist:
+        dpath = os.path.join(gamedir, d);
+        if (os.path.isdir(os.path.join(overridesloc, d)) and os.path.exists(dpath)):
+            #this is a directory which we are going to override. delete it
+            shutil.rmtree(dpath);
+    time.sleep(2);
+
+    #make sure the mods folder is empty and exists
+    gamemodsdir = os.path.join(gamedir, "mods");
+    if (os.path.exists(gamemodsdir)):
+        shutil.rmtree(gamemodsdir);
+        time.sleep(2);
+    #the mods folder does not exist, so make it
+    os.mkdir(gamemodsdir);
+
+    #now copy all the things
+    print("\tInstalling base mods");
+    if (os.path.exists(modsLoc)):
+        #shutil.copy(modsLoc, gamedir);
+        CopyReplaceFile(modsLoc, gamemodsdir);
+    else:
+        print("OI. DON'T TOUCH MY MODS.");
+        sys.exit();
+
+    print("\tInstalling overrides");
+    #move contents of the overrides folder, not the folder itself
+    #files = os.listdir(overridesloc)
+    #for f in files:
+        #shutil.copy(os.path.join(overridesloc, f), gamedir);
+    CopyReplaceFile(overridesloc, gamedir);
+    print("Done!");
+
+    print("\nClean up temporary installation data? (y/n)\n(If this data is not cleaned up you can reinstall the modpack without redownloading by running \""+sys.argv[0]+" '"+sys.argv[1]+"' -nozip -nodown -noforge\")");
+    while (True):
+        t = input();
+        if (t == "y" or t == "Y"):
+            #cleanup then break
+            print("\tRemoving '" +tempDir+"'");
+            shutil.rmtree(tempDir);
+            print("\tDone!");
+            break;
+        elif (t == "n" or t == "N"):
+            print("\tNo Cleanup");
+            break;
+        else:
+            continue;
+
+
+    print(name + " has been installed - Enjoy!");
+
+#copy everything in srcpath to dstpath
+#both paths are directories and exist
+#creates directories as needed and replaces any existing files
+#existing directories are merged into
+def CopyReplaceFile(srcpath, dstpath):
+    contents = os.listdir(srcpath);
+    for file in contents:
+        srcfilepath = os.path.join(srcpath, file);
+        dstfilepath = os.path.join(dstpath, file);
+        if (os.path.isdir(srcfilepath)):
+            #recurse using this folder
+            if (not os.path.exists(dstfilepath)):
+                os.mkdir(dstfilepath);
+            CopyReplaceFile(srcfilepath, dstfilepath);
+        else:
+            #this is a file we need to copy. Check if the dest exists and delete if needed
+            if (os.path.exists(dstfilepath)):
+                os.remove(dstfilepath);
+            shutil.copy2(srcfilepath, dstfilepath)
+
+class DownloadThreadData():
+    def __init__(self, fileList, errorList, fileListLock, errorListLock):
+        self.fileList = fileList;
+        self.errorList = errorList;
+        self.fileListLock = fileListLock;
+        self.errorListLock = errorListLock;
+        self.filePos = 0;
+        self.fileDone = 0;
+        self.fileCount = len(self.fileList);
+
+def DownloadModsThread(data):
+    while (True):
+        #get the next mod in the list and download it
+        with data.fileListLock:
+            if (data.filePos >= data.fileCount):
+                break;
+            else:
+                #there is a mod to download, get it
+                currFile = data.fileList[data.filePos];
+                filePos = data.filePos;
+                data.filePos += 1
+        #should only get here if there is actually a mod to download
+        (error, resultString) = DownloadMod(currFile[0], currFile[1]);
+        if (error):
+            with data.errorListLock:
+                data.errorList.append(f"({filePos})\t{resultString}")
+        #clear the line then display the result string to the user
+        print(' '*50, end='\r')
+        print(resultString)
+        with data.fileListLock:
+            data.fileDone += 1
+            
+
+#(error, reason)
+def DownloadMod(projectID, fileID):
+    #first aquire the download link from the curseforge interface
+    interfaceurl = f"https://addons-ecs.forgesvc.net/api/v2/addon/{projectID}/file/{fileID}/download-url"
+    (error, result) = downloadURL(interfaceurl)
+    if (error):
+        return (True, f"An issue was encountered when trying to retrieve a download link\n\t{result}")
+
+    #result should contain the result object with the link to the mod file
+    link = result.read().decode('utf-8')
+    match = re.findall(r"^(https?://)(.*)", link)
+    downloadLink = match[0][0] + urllib.parse.quote(match[0][1])
+    (error, result) = downloadURL(downloadLink)
+    if (error):
+        return (True, f"An issue was encountered when trying download a mod\n\t{result}")
+
+
+    #we got the mod boyz, lets get to writing files!
+    modName = re.findall(r'[^/]*$', result.geturl())[0];
+    modName = urllib.parse.unquote(modName);
+    #print(str(count) + "/" + str(num) + "\t" + modName);
+    modName = re.sub(r'\\/:\*\?"<>\|', "-", modName);
+    modF = open(os.path.join(modsLoc, modName), "wb");
+    modF.write(result.read());
+    modF.close();
+    return (False, f"{modName}");
+
+#given a url, will try and download it, returns (error, requestobject/string)
+def downloadURL(url):
+    #im not a bot lol
+    agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
+    tries = 0;
+    rec = urllib.request.Request(url, headers={'User-Agent': agent});
+    
+    while (tries < 3):
+        try:
+            rec = urllib.request.Request(url, headers={'User-Agent': agent});
+            r = urllib.request.urlopen(rec);
+            return (False, r)
+        except urllib.error.HTTPError as e:
+            return (True, f"HTTP ERROR {e.code}: {url}")
+        except:
+            tries += 1;
+
+    if (tries >= 3):
+        return (True, f"Probably timed out: {url}");
+
+
+if __name__ == '__main__':
+    main();
---
+++ README.md
@@ -0,0 +1,20 @@
+# Minecraft Curseforge Modpack Installer
+Do you want to play a curseforge modpack but do not want to be forced to sign up for a new account in order to download and install it? What if the whole installation process could be performed by a single Python script? Then look no further, the solution is right here! Initially intended as a challenge project (and as a "I do not want to sign up for a new account for something which is freely available"), this script will take a modpack zip file, unpack it, read its manifest, download all the required mods and forge, install them, and create a new game profile in your Minecraft launcher. NOTE: This script assumes you have a Windows machine and your Minecraft installation is in the usual place of: `%appdata%/.minecraft`. If not, you will need to exercise some coding skills and change the location of installation.
+
+## How to use
+1. Go to curse forge and find a Minecraft modpack you like the look of.
+2. Click the files tab (near the top left of the page).
+3. Click on the NAME of the version of the modpack you want to install. If you just want the latest one, then it will be the top one. Otherwise, find your prefered version and click it.
+4. To the right of the file's name there should be a download button, click that and download the zip file.
+5. Place the downloaded zip file in the same folder as the python script `InstallModPack.py` (you have downloaded the script by now, right?).
+6. Open your preferred console to the location of the script. (if you do not know what this means, open command prompt, copy the location of the folder the script is in and write in command prompt `cd <paste location here>`).
+7. Now you can run the install script. Take note of the name of the zip file you downloaded and run the following command in your console. If you don't have python installed already, you will need to go and install that first.\
+`python InstallModPack.py <zip file name here>`
+8. Follow the prompts in the console until the install completes.
+9. Open your Minecraft launcher, a new profile called something like "modpack - <modname>" should be available to play. 10. Aaaaand you are done!
+
+If you wish to skip certain parts of the installation process (eg. you want to reinstall a modpack that was recently downloaded but you messed with the files and it does not work anymore), you can add any of the following flags after the run command\
+`-nozip` Skips unzipping the specified modpack file and refreshing the temp folder (use when you already unzipped the mod before and want to perform a reinstall without having to redownload the mods).\
+`-nodown` Skips downloading the required mods. This is usually used in combination with the above for a quick reinstall.\
+`-noforge` Skips downloading and installation of forge. This can be used when you have already installed the required version of forge.\
+`-noprofile` Skips setting up of a new Minecraft game profile for the modpack. Generally, you will not use this unless you know what you are doing.
